3d07a0cd39822c89a62e63cf980fd18ef9f7445f
jdbbaugh <jbaugh78@msn.com>
2018-12-10 07:43:36 -0600

    completed coinsToCash practice

--- coinsToCash.js
+++ coinsToCash.js
@@ -2,13 +2,11 @@ const piggyBank = {
     quarters: 4, 
     nickles: 5, 
     dimes: 7, 
-    pennies: 89
+    pennies: 90
 };
 console.log(piggyBank.quarters)
 let dollarAmount = (piggyBank.quarters * .25) + (piggyBank.nickles * .05) + (piggyBank.dimes  * .10) + (piggyBank.pennies * .01)
 console.log(`$${dollarAmount.toFixed(2)}`);
 
 
-const dollarAmount2 = 2
-const piggyBank2 = {}
 
